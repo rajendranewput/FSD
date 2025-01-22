21b0f68aac0778f318dfab9f8470c4f2a10a1e19
rajendranewput <rajendra@newput.com>
2025-01-22 17:46:07 +0530

    fixing CORS

--- app/Http/Middleware/OwnCors.php
+++ app/Http/Middleware/OwnCors.php
@@ -17,20 +17,11 @@ class OwnCors
      */
     public function handle(Request $request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-
-        $headers = [
-            'Access-Control-Allow-Methods' => '*',
-            'Access-Control-Allow-Headers' => '*'
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            return response('OK')
-                ->withHeaders($headers);
-        }
-
-        $response = $next($request);
-        foreach ($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', '*')
+        ->header('Access-Control-Allow-Credentials', true)
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+        ->header('Accept', 'application/json');
     }
 }
